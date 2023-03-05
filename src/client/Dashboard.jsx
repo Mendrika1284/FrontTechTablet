6a13fd57646c5583b2413e3db86a55a3c9460cb8
Mendrika Itokiana <mendrikaitokiuana@gmail.com>
2023-03-05 10:52:04 +0300

    Update Dashboard.jsx

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -116,24 +116,23 @@ const Dashboard = ({ setCount }) => {
                                                 <form onSubmit={handleFormSubmit(product)}>
                                                     <div className="card product-item border-0 mb-4">
                                                         <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                                            <img className="img-fluid w-100" src={product.pictures} alt="" />
+                                                            <img className="img-fluid w-100" style={{height: "600px"}} src={product.pictures} alt="" />
                                                         </div>
                                                         <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                                            <h6 className="text-truncate mb-3">{product.id}</h6>
                                                             <h6 className="text-truncate mb-3">{product.name}</h6>
-                                                            <div className="col-sm-3 ">
-                                                                <input type="number" name="quantite" className="form-control d-flex justify-content-center" defaultValue={localStorage.getItem(`card-${product.id}-quantity`) || "Quantité"} id="inlineFormInputName" min="0" placeholder="Quantité" onChange={(e) => {
+                                                            <h6 className="text-truncate mb-3">{product.color.name}</h6>
+                                                            <div className="d-flex justify-content-center">
+                                                                <h6>{product.pricettc} €</h6>   
+                                                            </div>
+                                                            <div className="col-sm-3 d-flex justify-content-center">
+                                                                <input type="number" name="quantite" className="form-control" defaultValue={localStorage.getItem(`card-${product.id}-quantity`) || "Quantité"} id="inlineFormInputName" min="0" placeholder="Quantité" onChange={(e) => {
                                                                     const cardId = product.id
                                                                     localStorage.setItem(`card-${cardId}-quantity`, e.target.value);
                                                                 }} />
                                                             </div>
-                                                            <div className="d-flex justify-content-center">
-                                                                <h6>{product.pricettc} €</h6>   
-                                                            </div>
                                                         </div>
-                                                        <div className="card-footer d-flex justify-content-between bg-light border">
-                                                            <a href="" className="btn btn-sm text-dark p-0"><i className="fas fa-eye text-primary mr-1"></i>View Detail</a>
-                                                            <button className="btn btn-sm text-dark p-0"><i className="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</button>
+                                                        <div className="card-footer d-flex justify-content-center bg-light border">
+                                                            <button className="btn btn-sm text-dark p-0"><i className="fas fa-shopping-cart text-primary mr-1"></i>Ajouter aux panier</button>
                                                         </div>
                                                     </div>
                                                 </form>
